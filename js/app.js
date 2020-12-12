ad9dea69f02ef6590009cb337590d74c565d550b
tediko <tediko123@wp.pl>
2020-12-12 12:20:18 +0100

    Disabled lazyLoadAnimations

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 import { default as formValidation } from './formValidation.js';
 import { default as lazyLoad } from './lazyLoadAnimations.js';
 new formValidation();
-new lazyLoad();
+/* new lazyLoad(); */
